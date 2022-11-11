06cd1fe34a5f5eafa807454b1d18d2a18cfa8773
Tomás Belmar <tomasbelmarcosta@gmail.com>
2022-11-11 17:31:10 +0100

    finished lab2. still need to return count from thread func

---
+++ module2/threads.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+pthread_t ids[3];
+int buf = 0;
+pthread_mutex_t lock;
+
+void *work()
+{
+    int count = 0;
+    while (1)
+    {
+        pthread_mutex_lock(&lock);
+
+        if (buf < 15)
+        {
+            printf("TID: %lu, PID: %d, Buffer: %d\n", pthread_self(), getpid(), buf++);
+            count++;
+
+            pthread_mutex_unlock(&lock);
+            sleep(0.01);
+        }
+        else
+        {
+            pthread_mutex_unlock(&lock);
+            printf("TID %lu worked on the buffer %d \033[0;32mtimes\033[0m\n", pthread_self(), count);
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        // printf("thread created %d\n", i);
+        pthread_create(&(ids[i]), NULL, &work, NULL);
+    }
+
+    pthread_join(ids[0], NULL);
+    pthread_join(ids[1], NULL);
+    pthread_join(ids[2], NULL);
+
+    printf("Total buffer accesses: %d\n", buf);
+
+    return 0;
+}
