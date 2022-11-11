5de1899f4b7858837c92ae4a366d018d7375d542
Tomás Belmar <tomasbelmarcosta@gmail.com>
2022-11-11 17:52:36 +0100

    finished module 2

--- module2/threads.c
+++ module2/threads.c
@@ -8,7 +8,7 @@ pthread_mutex_t lock;
 
 void *work()
 {
-    int count = 0;
+    void *count = 0;
     while (1)
     {
         pthread_mutex_lock(&lock);
@@ -24,8 +24,7 @@ void *work()
         else
         {
             pthread_mutex_unlock(&lock);
-            printf("TID %lu worked on the buffer %d \033[0;32mtimes\033[0m\n", pthread_self(), count);
-            break;
+            return (void *)count;
         }
     }
 }
@@ -39,9 +38,17 @@ int main()
         pthread_create(&(ids[i]), NULL, &work, NULL);
     }
 
-    pthread_join(ids[0], NULL);
-    pthread_join(ids[1], NULL);
-    pthread_join(ids[2], NULL);
+    int count1;
+    int count2;
+    int count3;
+
+    pthread_join(ids[0], (void **)&count1);
+    pthread_join(ids[1], (void **)&count2);
+    pthread_join(ids[2], (void **)&count3);
+
+    printf("TID %lu worked on the buffer %d \033[0;32mtimes\033[0m\n", pthread_self(), count1);
+    printf("TID %lu worked on the buffer %d \033[0;32mtimes\033[0m\n", pthread_self(), count2);
+    printf("TID %lu worked on the buffer %d \033[0;32mtimes\033[0m\n", pthread_self(), count3);
 
     printf("Total buffer accesses: %d\n", buf);
 
