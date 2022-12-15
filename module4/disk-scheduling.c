44cf093ed1a9ef971464863876f6439499c1b632
Tomás Belmar <tomasbelmarcosta@gmail.com>
2022-12-15 18:31:23 +0100

    Fixed C-Look and Look

--- module4/disk-scheduling.c
+++ module4/disk-scheduling.c
@@ -4,11 +4,27 @@
 #include <time.h>
 
 #define NUM_OF_REQUESTS 1000
-#define UPPER_BOUND 4999
 #define DISK_SIZE 5000
 
 int disk[DISK_SIZE];
 int initialHead;
+
+void insertionSort(int *array)
+{
+    int i, key, j;
+    for (i = 1; i < NUM_OF_REQUESTS; i++)
+    {
+        key = array[i];
+        j = i - 1;
+
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+}
 
 // first come first serve
 int fcfs(int *requests, int head)
@@ -44,7 +60,7 @@ int sstf(int *requests, int head)
     for (int i = 0; i < NUM_OF_REQUESTS; i++)
     {
         int closestIndex = -1;
-        int shortestLen = UPPER_BOUND + 1;
+        int shortestLen = DISK_SIZE + 1;
 
         // find shortest seek time value in the requests
         for (int j = 0; j < NUM_OF_REQUESTS; j++)
@@ -77,7 +93,9 @@ int scan(int *requests, int head)
     // copy requests into identical array
     int requestsCopy[NUM_OF_REQUESTS];
     for (int i = 0; i < NUM_OF_REQUESTS; i++)
+    {
         requestsCopy[i] = requests[i];
+    }
 
     int originalHead = head;
 
@@ -99,7 +117,7 @@ int scan(int *requests, int head)
     numberOfMovements += head;
     head = 0;
 
-    for (int i = originalHead + 1; i < UPPER_BOUND; i++)
+    for (int i = originalHead + 1; i < DISK_SIZE; i++)
     {
         for (int j = 0; j < NUM_OF_REQUESTS; j++)
         {
@@ -128,7 +146,7 @@ int cscan(int *requests, int head)
     for (int i = 0; i < NUM_OF_REQUESTS; i++)
         requestsCopy[i] = requests[i];
 
-    for (int i = originalHead; i < UPPER_BOUND; i++)
+    for (int i = originalHead; i < DISK_SIZE; i++)
     {
         for (int j = 0; j < NUM_OF_REQUESTS; j++)
         {
@@ -143,9 +161,9 @@ int cscan(int *requests, int head)
     }
 
     // C-SCAN moves the head to the end of the disk, goes back to the beginning, and then continues normally
-    numberOfMovements += UPPER_BOUND - head;
+    numberOfMovements += (DISK_SIZE - 1) - head;
     // I'm not sure if we're meant to count the jump from end to beginning. If so, uncomment this line
-    // numberOfMovements += UPPER_BOUND;
+    // numberOfMovements += DISK_SIZE;
     head = 0;
 
     for (int i = 0; i < originalHead; i++)
@@ -173,36 +191,31 @@ int look(int *requests, int head)
     // copy requests into identical array
     int requestsCopy[NUM_OF_REQUESTS];
     for (int i = 0; i < NUM_OF_REQUESTS; i++)
-        requestsCopy[i] = requests[i];
-
-    int originalHead = head;
-
-    for (int i = originalHead; i < UPPER_BOUND; i++)
     {
-        for (int j = 0; j < NUM_OF_REQUESTS; j++)
-        {
-            // found the number
-            if (requestsCopy[j] == i)
-            {
-                numberOfMovements += abs(head - i);
-                head = i;
-                break;
-            }
-        }
+        requestsCopy[i] = requests[i];
     }
 
-    for (int i = 0; i < originalHead; i++)
+    insertionSort(requestsCopy);
+
+    int k = 0;
+    int originalHead = head;
+
+    // Find index to start at
+    while (originalHead > requestsCopy[k])
     {
-        for (int j = 0; j < NUM_OF_REQUESTS; j++)
-        {
-            // found the number
-            if (requestsCopy[j] == i)
-            {
-                numberOfMovements += abs(head - i);
-                head = i;
-                break;
-            }
-        }
+        k++;
+    }
+
+    for (int i = k; i < NUM_OF_REQUESTS; i++)
+    {
+        numberOfMovements += abs(head - requestsCopy[i]);
+        head = requestsCopy[i];
+    }
+
+    for (int i = k - 1; i >= 0; i--)
+    {
+        numberOfMovements += abs(head - requestsCopy[i]);
+        head = requestsCopy[i];
     }
 
     return numberOfMovements;
@@ -219,34 +232,30 @@ int clook(int *requests, int head)
     // copy requests into identical array
     int requestsCopy[NUM_OF_REQUESTS];
     for (int i = 0; i < NUM_OF_REQUESTS; i++)
-        requestsCopy[i] = requests[i];
-
-    for (int i = originalHead; i < UPPER_BOUND; i++)
     {
-        for (int j = 0; j < NUM_OF_REQUESTS; j++)
-        {
-            // found the number
-            if (requestsCopy[j] == i)
-            {
-                numberOfMovements += abs(head - i);
-                head = i;
-                break;
-            }
-        }
+        requestsCopy[i] = requests[i];
     }
 
-    for (int i = 0; i < originalHead; i++)
+    insertionSort(requestsCopy);
+
+    int k = 0;
+
+    // Find index to start at
+    while (originalHead > requestsCopy[k])
     {
-        for (int j = 0; j < NUM_OF_REQUESTS; j++)
-        {
-            // found the number
-            if (requestsCopy[j] == i)
-            {
-                numberOfMovements += abs(head - i);
-                head = i;
-                break;
-            }
-        }
+        k++;
+    }
+
+    for (int i = k; i < NUM_OF_REQUESTS; i++)
+    {
+        numberOfMovements += abs(head - requestsCopy[i]);
+        head = requestsCopy[i];
+    }
+
+    for (int i = 0; i <= k; i++)
+    {
+        numberOfMovements += abs(head - requestsCopy[i]);
+        head = requestsCopy[i];
     }
 
     return numberOfMovements;
@@ -268,7 +277,7 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < NUM_OF_REQUESTS; i++)
     {
-        int r = (rand() % UPPER_BOUND);
+        int r = (rand() % DISK_SIZE);
         requests[i] = r;
     }
 
